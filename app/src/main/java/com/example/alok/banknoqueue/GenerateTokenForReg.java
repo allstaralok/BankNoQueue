8c3b0c57f7d58970525ccf1e185b36806dbb5798
Alok <allstaralok@gmail.com>
2017-05-14 01:35:35 +0530

    Changes for Profile Registered user

--- app/src/main/java/com/example/alok/banknoqueue/GenerateTokenForReg.java
+++ app/src/main/java/com/example/alok/banknoqueue/GenerateTokenForReg.java
@@ -1,19 +1,74 @@
 package com.example.alok.banknoqueue;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class GenerateTokenForReg extends AppCompatActivity {
+
+    //Firebase Items
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
+
+    private Spinner branchSpinner;
+    private Spinner serviceSpineer;
+
+    private Button genToken;
+
+
+    final String TAG = "GenerateTokenForReg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generate_token_for_reg);
+
+        Intent intent = getIntent();
+        final String UserId = intent.getStringExtra("UserId");
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        branchSpinner = (Spinner) findViewById(R.id.branchSpinner);
+        serviceSpineer = (Spinner) findViewById(R.id.serviceSpinner);
+        genToken = (Button) findViewById(R.id.generateToken);
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                final DataSnapshot user = dataSnapshot.child("USER").child(UserId);
+                final DataSnapshot bank = dataSnapshot.child("BRANCHES");
+
+                genToken.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        generateToken(user,bank,UserId);
+                    }
+                });
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
-/*
+
 
     public void generateToken(DataSnapshot user, DataSnapshot branch, String userID)
     {
@@ -21,9 +76,9 @@ public class GenerateTokenForReg extends AppCompatActivity {
         if(user.getValue(UserInformation.class).getTokenInd().toString().equals("N")  )
         {
             //User Data
-            String BranchName = mBanchSpinner.getSelectedItem().toString();
+            String BranchName = branchSpinner.getSelectedItem().toString();
             String Name = user.getValue(UserInformation.class).getName().toString();
-            String Service = user.getValue(UserInformation.class).getService().toString();
+            String Service = serviceSpineer.getSelectedItem().toString();
 
             //Branch Data
             branch = branch.child(BranchName);
@@ -32,13 +87,20 @@ public class GenerateTokenForReg extends AppCompatActivity {
             int token =  Integer.parseInt(tokenno);
             token++;
 
+            if(token < 50) {
+                //Start Updation
+                UserInformation userInformation = new UserInformation(BranchName, Name, Service, "Y", String.valueOf(token), String.valueOf("1"));
+                Branch branch1 = new Branch(String.valueOf("1"), String.valueOf(token));
+                databaseReference.child("USER").child(userID).setValue(userInformation);
+                databaseReference.child("BRANCHES").child(BranchName).setValue(branch1);
+                finish();
+                Intent intent = new Intent(GenerateTokenForReg.this,Profile.class);
+                startActivity(intent);
+            }
 
-            //Start Updation
-            UserInformation userInformation = new UserInformation(BranchName,Name,"Service","Y",String.valueOf(token),String.valueOf("1"));
-            Branch branch1 = new Branch(String.valueOf("1"),String.valueOf(token));
-            databaseReference.child("USER").child(userID).setValue(userInformation);
-            databaseReference.child("BRANCHES").child(BranchName).setValue(branch1);
-
+            else {
+                Toast.makeText(this,"Queue Full For Today Try Next Day",Toast.LENGTH_LONG).show();
+            }
 
             Log.d(TAG,BranchName + " "+ Name + " " + Service + " "+ user.getValue(UserInformation.class).getTokenInd().toString() + " "
                     + tokenno
@@ -50,5 +112,5 @@ public class GenerateTokenForReg extends AppCompatActivity {
 
         }
 
-    }*/
+    }
 }
--- app/src/main/java/com/example/alok/banknoqueue/Profile.java
+++ app/src/main/java/com/example/alok/banknoqueue/Profile.java
@@ -1,5 +1,6 @@
 package com.example.alok.banknoqueue;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -33,13 +34,25 @@ public class Profile extends AppCompatActivity {
     private TextView serviceTextView;
     private TextView welcome;
 
+    private TextView token;
+    private TextView branch;
+    private TextView service;
+    private TextView time;
+
     private Button signout;
     private Button update;
+
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        token = (TextView) findViewById(R.id.token);
+        branch = (TextView) findViewById(R.id.branch);
+        service = (TextView) findViewById(R.id.service);
+        time = (TextView) findViewById(R.id.time);
 
         tokenNumber = (TextView) findViewById(R.id.tokenCountTextView);
         update = (Button) findViewById(R.id.update);
@@ -47,6 +60,10 @@ public class Profile extends AppCompatActivity {
         branchName = (TextView) findViewById(R.id.branchNameTextView);
         serviceTextView = (TextView) findViewById(R.id.serviceTextView);
         welcome = (TextView) findViewById(R.id.welcomeMessageTextView);
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Getting Data, Please Wait !");
+        progressDialog.show();
 
 
 
@@ -76,23 +93,40 @@ public class Profile extends AppCompatActivity {
                 final DataSnapshot bank = dataSnapshot.child("BRANCHES");
                 Log.d(TAG, "Value is--> " + user.getValue(UserInformation.class).getName());
                 Log.d(TAG,"Token Ind" + user.getValue(UserInformation.class).getTokenInd().toString());
+                userIdText.setText("Welcome " + user.getValue(UserInformation.class).getName());
 
                 if(user.getValue(UserInformation.class).getTokenInd().toString().equals("Y")) {
-                    userIdText.setText("Welcome " + user.getValue(UserInformation.class).getName());
+                    progressDialog.dismiss();
                     branchName.setText(user.getValue(UserInformation.class).getBranch());
                     tokenNumber.setText(user.getValue(UserInformation.class).getTokenNo());
                     serviceTextView.setText(user.getValue(UserInformation.class).getService());
                     welcome.setText("Your Ticket Details Are as Follows:");
                     update.setVisibility(View.INVISIBLE);
                 }
+                else {
+                    //Hiding Unwanted Item
 
-                update.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                    //    generateToken(user,bank,userID);
-                    }
-                });
+                    branchName.setVisibility(View.INVISIBLE);
+                    tokenNumber.setVisibility(View.INVISIBLE);
+                    serviceTextView.setVisibility(View.INVISIBLE);
+                    branch.setVisibility(View.INVISIBLE);
+                    service.setVisibility(View.INVISIBLE);
+                    token.setVisibility(View.INVISIBLE);
+                    time.setVisibility(View.INVISIBLE);
 
+                    progressDialog.dismiss();
+
+                    update.setOnClickListener(new View.OnClickListener() {
+                                                  @Override
+                                                  public void onClick(View v) {
+                                                      Intent intent = new Intent(Profile.this, GenerateTokenForReg.class);
+                                                      intent.putExtra("UserId",userID);
+                                                      finish();
+                                                      startActivity(intent);
+                                                  }
+                                              }
+                    );
+                }
 
             }
 
